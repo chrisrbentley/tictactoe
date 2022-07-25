38c2fc6200fac4c27d1d19aec4e86ecdbacfd933
Chris Bentley <chrisrbentley@pm.me>
2022-07-24 21:05:21 -0400

    Added game board module, player factory, players, and game flow factory.

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ const gameBoard = (() => {
     let board = [];
 
     return { board }
-})()
+})();
+
+const player = (name) => {
+    return { name }
+};
+
+const gameFlow = () => {
+
+};
+
+
+const playerX = player('X');
+const playerO = player('O');
