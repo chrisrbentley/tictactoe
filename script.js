62f98b3256712f14ec16e7171da6500515d077aa
Chris Bentley <chrisrbentley@pm.me>
2022-07-25 15:03:58 -0400

    Added game board in HTML with JS.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-    let board = [];
+    let board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
 
     return { board }
 })();
@@ -12,6 +12,23 @@ const gameFlow = () => {
 
 };
 
-
 const playerX = player('X');
 const playerO = player('O');
+
+// create div container
+
+const gameContainer = document.querySelector('#game-container');
+
+let counter = 1
+for (i = 0; i < 9; i++) {
+    const gameSquare = document.createElement('div');
+    gameSquare.className = 'square';
+    gameContainer.appendChild(gameSquare);
+    gameSquare.id = counter;
+    counter++;
+}
+
+
+
+
+
