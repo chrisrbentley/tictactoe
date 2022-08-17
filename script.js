4663b70abf787d8e2df20dc06834b3dbb5adf0ed
Chris Bentley <chrisrbentley@pm.me>
2022-08-17 16:52:32 -0400

    Added logic that checks if the game is over, and restarts when it is.

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ const Game = (() => {
     let playerTwo = Player('Two', 'O');
     let currentPlayer = playerOne;
     let cell = document.querySelectorAll('.cell');
+    const body = document.querySelector('#body');
+
 
     cell.forEach((cell) => {
         cell.addEventListener('click', () => {
@@ -38,6 +40,89 @@ const Game = (() => {
                         cell.textContent = Gameboard.board[chosenCell];
                     }
                 })();
+
+                function checkWinner() {
+                    if (Gameboard.board[0] === 'X' && Gameboard.board[1] === 'X' && Gameboard.board[2] === 'X' || 
+                        Gameboard.board[3] === 'X' && Gameboard.board[4] === 'X' && Gameboard.board[5] === 'X' || 
+                        Gameboard.board[6] === 'X' && Gameboard.board[7] === 'X' && Gameboard.board[8] === 'X' ||
+                        Gameboard.board[0] === 'X' && Gameboard.board[3] === 'X' && Gameboard.board[6] === 'X' ||
+                        Gameboard.board[1] === 'X' && Gameboard.board[4] === 'X' && Gameboard.board[7] === 'X' || 
+                        Gameboard.board[2] === 'X' && Gameboard.board[5] === 'X' && Gameboard.board[8] === 'X' || 
+                        Gameboard.board[0] === 'X' && Gameboard.board[4] === 'X' && Gameboard.board[8] === 'X' ||
+                        Gameboard.board[2] === 'X' && Gameboard.board[4] === 'X' && Gameboard.board[6] === 'X') {
+
+                            console.log('Player One wins!');
+                            const result = document.createElement('p');
+                            body.appendChild(result);
+                            result.textContent = 'Player One wins!';
+                            const restartBtn = document.createElement('button');
+                            body.appendChild(restartBtn);
+                            restartBtn.textContent = 'Play again';
+
+                            restartBtn.addEventListener('click', () => {
+                                Gameboard.board.fill('');
+                                console.log('111');
+                                document.querySelectorAll('.cell').forEach(cell => {
+                                    cell.textContent = Gameboard.board[i];
+                                })
+                                restartBtn.remove();
+                                result.remove();
+                                currentPlayer = playerOne;
+                            });
+
+                    } else if (Gameboard.board[0] === 'O' && Gameboard.board[1] === 'O' && Gameboard.board[2] === 'O' || 
+                    Gameboard.board[3] === 'O' && Gameboard.board[4] === 'O' && Gameboard.board[5] === 'O' || 
+                    Gameboard.board[6] === 'O' && Gameboard.board[7] === 'O' && Gameboard.board[8] === 'O' ||
+                    Gameboard.board[0] === 'O' && Gameboard.board[3] === 'O' && Gameboard.board[6] === 'O' ||
+                    Gameboard.board[1] === 'O' && Gameboard.board[4] === 'O' && Gameboard.board[7] === 'O' || 
+                    Gameboard.board[2] === 'O' && Gameboard.board[5] === 'O' && Gameboard.board[8] === 'O' || 
+                    Gameboard.board[0] === 'O' && Gameboard.board[4] === 'O' && Gameboard.board[8] === 'O' ||
+                    Gameboard.board[2] === 'O' && Gameboard.board[4] === 'O' && Gameboard.board[6] === 'O') {
+
+                        console.log('Player Two wins!');
+                        const result = document.createElement('p');
+                        const body = document.querySelector('#body');
+                        body.appendChild(result);
+                        result.textContent = 'Player Two wins!';
+                        const restartBtn = document.createElement('button');
+                        body.appendChild(restartBtn);
+                        restartBtn.textContent = 'Play again';
+
+                        restartBtn.addEventListener('click', () => {
+                                Gameboard.board.fill('');
+                                console.log('111');
+                                document.querySelectorAll('.cell').forEach(cell => {
+                                    cell.textContent = Gameboard.board[i];
+                                })
+                                restartBtn.remove();
+                                result.remove();
+                                currentPlayer = playerOne;
+                            });
+
+                    } else if (Gameboard.board[0] != '' && Gameboard.board[1] != '' && Gameboard.board[2] != '' 
+                    && Gameboard.board[3] != '' && Gameboard.board[4] != '' && Gameboard.board[5] != '' 
+                    && Gameboard.board[6] != '' && Gameboard.board[7] != '' && Gameboard.board[8] != '') {
+                        console.log('It\'s a tie!');
+                        const result = document.createElement('p');
+                        body.appendChild(result);
+                        result.textContent = 'It\'s a tie!';
+                        const restartBtn = document.createElement('button');
+                        body.appendChild(restartBtn);
+                        restartBtn.textContent = 'Play again';
+                        
+                        restartBtn.addEventListener('click', () => {
+                            Gameboard.board.fill('');
+                            console.log('111');
+                            document.querySelectorAll('.cell').forEach(cell => {
+                                cell.textContent = Gameboard.board[i];
+                            })
+                            restartBtn.remove();
+                            result.remove();
+                            currentPlayer = playerOne;
+                        });
+                    }
+                }
+                checkWinner();
                 
                 if (currentPlayer === playerOne) {
                     currentPlayer = playerTwo;
@@ -45,14 +130,15 @@ const Game = (() => {
                     currentPlayer = playerOne;
                 }
                 
-                return displayController
+                return { displayController }
             } else {
                 alert('That spot has already been taken! Try another spot.');
             }
 
-            return { chosenCell };
+            return { chosenCell, checkWinner };
         })
     });
+
 
     return { playerOne, playerTwo, currentPlayer }
 })();
